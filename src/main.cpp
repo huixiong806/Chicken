34d46755e46ea32bb230476031a4ae2e9cd92436
Leozhu123 <570727732@qq.com>
2018-04-12 18:07:25 +0800

    可以无限训练

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@ int main()
 	fann_randomize_weights(ann, -1, 1);
 	fann_save(ann, ".\\chicken.net");
 	*/
+	int count_train=0;
 	bool outputStone = false;
 	ifstream config("config.ini");
 	if (config.is_open())
@@ -59,70 +60,75 @@ int main()
 	backup = cout.rdbuf();
 	cout.rdbuf(fout.rdbuf());
 	*/
-	srand(time(0));
-	Game<siz, siz> game;
-	if (outputStone) game.outputStone(); else game.output();
-	MonteCarloTreeSearchAI<siz, siz> ai;
-	int32_t color = 1;
-	int32_t winner = 0;
-	while (!game.gameOver())
-	{
-		cout << (color > 0 ? "Black's turn" : "White's turn") << endl;
-		size_t index;
-		if (color == myColor)
+	do{
+		count_move = 0;
+		srand(time(0));
+		Game<siz, siz> game;
+		if (outputStone) game.outputStone(); else game.output();
+		MonteCarloTreeSearchAI<siz, siz> ai;
+		int32_t color = 1;
+		int32_t winner = 0;
+		while (!game.gameOver())
 		{
-			int p; char q;
-			cin >> q >> p;
-			p = siz - p;
-			q = toupper(q) - 'A';
-			index = p * siz + q;
+			cout << (color > 0 ? "Black's turn" : "White's turn") << endl;
+			size_t index;
+			if (color == myColor)
+			{
+				int p; char q;
+				cin >> q >> p;
+				p = siz - p;
+				q = toupper(q) - 'A';
+				index = p * siz + q;
+			}
+			else {
+				index=ai.genMove(game, (Color)color, playout);// index = siz * siz + 2; siz * siz + 2 for fast play
+				if(index!=siz*siz+1) count_move++;
+			}
+			if (index == siz * siz + 1)
+			{
+				cout << (color > 0 ? "Black" : "White") << " resigned." << endl;
+				winner = -color;
+				break;
+			}
+			else if (index == siz * siz + 2)
+				game.fastPlay((Stone)color); // For debugging
+			else
+				game.play(index, (Stone)color);
+			if (outputStone)
+				game.outputStone();
+			else
+				game.output();
+			cout << endl;
+			color = -color;
 		}
-		else {
-			index=ai.genMove(game, (Color)color, playout);// index = siz * siz + 2; siz * siz + 2 for fast play
-			if(index!=siz*siz+1) count_move++;
-		}
-		if (index == siz * siz + 1)
-		{
-			cout << (color > 0 ? "Black" : "White") << " resigned." << endl;
-			winner = -color;
-			break;
-		}
-		else if (index == siz * siz + 2)
-			game.fastPlay((Stone)color); // For debugging
+		Color result = game.getResult();
+		if (winner == 0)
+			cout << (result == Color::BLACK ? "Black Win" : result == Color::WHITE ? "White Win" : "Tie") << endl;
 		else
-			game.play(index, (Stone)color);
-		if (outputStone)
-			game.outputStone();
-		else
-			game.output();
-		cout << endl;
-		color = -color;
-	}
-	Color result = game.getResult();
-	if (winner == 0)
-		cout << (result == Color::BLACK ? "Black Win" : result == Color::WHITE ? "White Win" : "Tie") << endl;
-	else
-		cout << (winner > 0 ? "Black Win" : "White Win") << endl;
-	network_train.close();
-	if (train){
-		string tstr;
+			cout << (winner > 0 ? "Black Win" : "White Win") << endl;
 		network_train.close();
-		std::ifstream inf(".\\train.tmp");
-		std::ofstream outf(".\\train.txt");
-		outf<<count_move<<" "<<675<<" "<<225<<endl;
-		while (!inf.eof()){
-			getline(inf,tstr);
-			outf<<tstr<<endl;
+		if (train){
+			string tstr;
+			network_train.close();
+			std::ifstream inf(".\\train.tmp");
+			std::ofstream outf(".\\train.txt");
+			outf<<count_move<<" "<<675<<" "<<225<<endl;
+			while (!inf.eof()){
+				getline(inf,tstr);
+				outf<<tstr<<endl;
+			}
+			outf.close();
+			inf.close();
+			struct fann *ann;
+			ann = fann_create_from_file(".\\chicken.net");
+			fann_set_training_algorithm(ann,FANN_TRAIN_BATCH);
+			cout << "Training" << endl;
+			fann_train_on_file(ann, ".\\train.txt", max_epochs,epochs_between_reports, desired_error);
+			cout << "Saving" <<endl;
+			fann_save(ann, ".\\chicken.net");
+			cout<<"Have trained:"<<++count_train<<endl;
 		}
-		outf.close();
-		inf.close();
-		struct fann *ann;
-		ann = fann_create_from_file(".\\chicken.net");
-		cout << "Training" << endl;
-		fann_train_on_file(ann, ".\\train.txt", max_epochs,epochs_between_reports, desired_error);
-		cout << "Saving" <<endl;
-		fann_save(ann, ".\\chicken.net");
-	}
+	}while(train);
 	cout << "press q to quit" << endl;
 	while (getchar() != 'q');
 	return 0;
